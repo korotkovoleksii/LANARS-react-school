1ea308259fff8907127bd3a3f91cceca67f49f16
alekseikorotkov <korotkovaleksei@icloud.com>
2022-11-24 17:47:02 +0200

    Implement interfaces for Album and Photo

---
+++ src/shared/interfaces/album.interface.ts
@@ -0,0 +1,7 @@
+export interface Album {
+  data: string;
+  description: string;
+  id: number;
+  photos: number[];
+  title: string;
+}
---
+++ src/shared/interfaces/photo.interface.ts
@@ -0,0 +1,8 @@
+export interface Photo {
+  date: string;
+  description: string | undefined;
+  id: number;
+  image: string;
+  size: number;
+  type: string;
+}
