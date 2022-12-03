9f308f4c1100ac575c9c567a0efa7b1bd88fd863
alekseikorotkov <korotkovaleksei@icloud.com>
2022-12-03 16:05:48 +0200

    Add photoSlice and connect store to app

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from 'shared/hooks/redux-hooks';
+import { retrievePhotos } from 'shared/store/Photos/photoSlice';
 const App = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  useEffect(()=>{
+    dispatch(retrievePhotos());
+  },[]);
   return <div className="App"></div>;
 };
 
---
+++ src/shared/store/Photos/photoSlice.ts
@@ -0,0 +1,79 @@
+import { IPhoto } from './../../interfaces/photo.interface';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import API from 'core/services/API';
+
+type PhotoSliceT = {
+  status: 'idle' | 'loading' | 'finished' | 'error';
+  data: IPhoto[];
+
+};
+
+export const retrievePhotos = createAsyncThunk(
+  'photos/retrieve',
+  async () => {
+    const res =  await API.get('/api/photos') as IPhoto[];
+    return res;
+  }
+);
+export const createPhoto = createAsyncThunk(
+  'photos/create',
+  async (photo: IPhoto) => {
+    const createdPhoto  = await API.post('/api/photos', photo) as IPhoto;
+    return createdPhoto;
+  }
+);
+export const updatePhoto = createAsyncThunk(
+  'photo/update',
+  async (photo: IPhoto) => {
+    const updatedPhoto = await API.patch('/api/photos',photo) as IPhoto;
+    return updatedPhoto;
+  }
+);
+export const deletePhoto = createAsyncThunk(
+  'photo/delete',
+  async (id: number) => {
+    await API.delete(`/api/photos?${id}`);
+    return {id};
+  }
+);
+
+const initialState: PhotoSliceT = {
+  status: 'idle',
+  data: [],
+
+};
+
+const photoSlice = createSlice({
+  name: '@@photo',
+  initialState,
+  reducers: {},
+  extraReducers:(bilder)=>{
+    bilder
+      .addCase(retrievePhotos.fulfilled,(state,action)=>{
+        state.status = 'finished';
+        state.data = action.payload;
+      })
+      .addCase(createPhoto.fulfilled, (state, action)=>{
+        state.data.push(action.payload);
+      })
+      .addCase(updatePhoto.fulfilled, (state, action)=>{
+        const index = state.data.findIndex(photo => photo.id === action.payload.id);
+        state.data[index]={
+          ...state.data[index],
+          ...action.payload,
+        };
+      })
+      .addCase(deletePhoto.fulfilled, (state,action)=>{
+        const index = state.data.findIndex(photo => photo.id === action.payload.id);
+        state.data.splice(index,1);
+      });
+
+
+  },
+
+});
+
+export default photoSlice.reducer;
+
+
