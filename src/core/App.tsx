6600230c2029ce94411a8eb709e06693d0788060
alekseikorotkov <korotkovaleksei@icloud.com>
2022-12-05 12:20:19 +0200

    Add interfaces for testing store

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,13 +1,42 @@
 import { useEffect } from 'react';
 import { useAppDispatch } from 'shared/hooks/redux-hooks';
-import { retrievePhotos } from 'shared/store/Photos/photoSlice';
+import { IPhoto } from 'shared/interfaces/photo.interface';
+import { retrievePhotos,deletePhoto,createPhoto  } from 'shared/store/Photos/photoSlice';
+// import { retrieveAlbum,deleteAlbum} from 'shared/store/Album/albumSlice';
+
 const App = (): JSX.Element => {
   const dispatch = useAppDispatch();
 
   useEffect(()=>{
+    // dispatch(retrievePhotos());
     dispatch(retrievePhotos());
+
   },[]);
-  return <div className="App"></div>;
+  const handlerClickDelete =(): void=>{
+    dispatch(deletePhoto(44));
+  };
+  const handlerClickUpdate = (): void=>{
+
+  };
+  const handlerClickCreate =(): void=>{
+
+    const someVal: Omit<IPhoto,'id'> = {
+      date: 'qqqqq',
+      description: 'qqqq',
+      image: 'qqqq',
+      size: 1,
+      type: 'qqqq',
+    };
+    dispatch(createPhoto(someVal));
+  };
+  return <div className="App">
+    <button onClick={handlerClickDelete}>delete photo id44
+    </button>
+    <button onClick={handlerClickCreate}>create photo</button>
+    <button onClick={handlerClickUpdate}>update photo</button>;
+         </div>;
+
+
 };
 
 export default App;
