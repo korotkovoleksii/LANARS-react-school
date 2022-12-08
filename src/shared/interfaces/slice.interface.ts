a68312383527ea7753b7da65fc58d1a9fa62aee3
alekseikorotkov <korotkovaleksei@icloud.com>
2022-12-08 17:27:03 +0200

    Change format slice.interface.ts

--- src/shared/interfaces/slice.interface.ts
+++ src/shared/interfaces/slice.interface.ts
@@ -1,5 +1,5 @@
-export interface IDataSlice<T>{
+export interface IDataSlice<T> {
   status: 'idle' | 'loading' | 'finished' | 'error';
   data: T[];
-  error: string|null;
+  error: string | null;
 }
