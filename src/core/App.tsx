17eaecb393b10c25440734e1a7c6cc2d9d6ef4da
alekseikorotkov <korotkovaleksei@icloud.com>
2022-12-27 17:14:58 +0200

    Implement  Header component and add to App

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,5 +1,19 @@
+import { Box, Divider, CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { appTheme } from '../styles/appTheme';
+import Header from 'modules/components/Header';
+import { colors } from 'styles/variables';
+
 const App = (): JSX.Element => {
-  return <div className="App"></div>;
+  return (
+    <Box>
+      <ThemeProvider theme={appTheme}>
+        <CssBaseline />
+        <Header />
+        <Divider sx={{ bgcolor: colors.light.divider }} />
+      </ThemeProvider>
+    </Box>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,16 @@ import App from './core/App';
 import reportWebVitals from './reportWebVitals';
 import Storage from 'core/services/back-end/Storage';
 import { firstInit } from 'core/services/fistInit';
+import '../src/styles/index.css';
 
 (async () => {
   await Storage.createObjectStore(['albums', 'photos']);
   firstInit();
-  const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-  );
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
   root.render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 
   // If you want to start measuring performance in your app, pass a function
