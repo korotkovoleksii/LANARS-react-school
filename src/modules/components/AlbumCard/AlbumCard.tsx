3954785d8fc1bc785a2dca30616a7e0410ff4b4d
alekseikorotkov <korotkovaleksei@icloud.com>
2023-01-06 16:14:43 +0200

    Add AlbumCard component

---
+++ src/modules/components/AlbumCard/AlbumCard.tsx
@@ -0,0 +1,100 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Box, Card, Typography, CardHeader, IconButton, CardMedia } from '@mui/material';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from 'shared/hooks/redux-hooks';
+import { retrievePhotos } from 'shared/store/Photos/photoSlice';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import CropOriginalOutlinedIcon from '@mui/icons-material/CropOriginalOutlined';
+import { colors } from 'styles/variables';
+
+const AlbumCard = ({ title, idPhoto, countPhotos }: { title: string; idPhoto: number; countPhotos: number }): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const photos = useAppSelector((store) => store.photo);
+
+  useEffect(() => {
+    dispatch(retrievePhotos([idPhoto]));
+  }, [dispatch]);
+
+  return (
+    <Card
+      sx={{
+        width: 280,
+        maxHeight: 370,
+        p: 0,
+
+        backgroundColor: 'transparent',
+        boxShadow: 'none',
+        position: 'relative',
+        '&:hover': {
+          '& .MuiIconButton-root': {
+            display: 'inline-flex',
+          },
+        },
+      }}
+    >
+      <CardHeader
+        sx={{
+          position: 'absolute',
+          right: 0,
+        }}
+        action={
+          <IconButton
+            aria-label="settings"
+            sx={{
+              color: colors.light.fabBtnText,
+              bgcolor: colors.light.fabBtnBG,
+              display: 'none',
+              '&:hover': {
+                bgcolor: colors.light.fabBtnBG,
+              },
+            }}
+          >
+            <MoreVertIcon />
+          </IconButton>
+        }
+      />
+      {photos.data.length > 1 ? (
+        <CardMedia
+          component="img"
+          height="280"
+          image={`data:image/jpeg;base64,${photos.data[0].image}`}
+          alt={title}
+          sx={{ borderRadius: '8px' }}
+        />
+      ) : (
+        <Box
+          height="280px"
+          sx={{
+            bgColor: colors.light.background,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            bgcolor: colors.light.background,
+            borderRadius: '8px',
+          }}
+        >
+          <CropOriginalOutlinedIcon
+            sx={{
+              color: colors.light.selectedBtnMenuBG,
+              fontSize: 160,
+            }}
+          />
+        </Box>
+      )}
+      <Box>
+        <Typography
+          sx={{
+            lineHeight: '24px',
+            maxHeight: '72px',
+            overflow: 'hidden',
+            whiteSpace: 'initial',
+          }}
+        >
+          {title}
+        </Typography>
+        <Typography variant="caption">{countPhotos} images</Typography>
+      </Box>
+    </Card>
+  );
+};
+export default AlbumCard;
