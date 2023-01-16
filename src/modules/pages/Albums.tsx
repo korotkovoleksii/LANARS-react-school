e7ee337fbd3c4b708e2d9aabca6e10b01aaf6d87
alekseikorotkov <korotkovaleksei@icloud.com>
2023-01-16 15:59:21 +0200

    Fix showing an empty state when the entity is loading

--- src/modules/pages/Albums.tsx
+++ src/modules/pages/Albums.tsx
@@ -5,6 +5,8 @@ import { useAppDispatch, useAppSelector } from 'shared/hooks/redux-hooks';
 import { retrieveAlbum } from 'shared/store/Album/albumSlice';
 import AlbumCard from 'modules/components/AlbumCard';
 import { useEffect } from 'react';
+import { clearPhotos } from 'shared/store/Photos/photoSlice';
+import { Status } from 'shared/helpers/statusRequstRTK';
 
 const Albums = (): JSX.Element => {
   const albums = useAppSelector((store) => store.album);
@@ -12,42 +14,50 @@ const Albums = (): JSX.Element => {
 
   useEffect(() => {
     dispatch(retrieveAlbum([]));
+    return () => {
+      dispatch(clearPhotos());
+    };
   }, [dispatch]);
   return (
     <>
-      {albums.data.length === 0 ? (
-        <Box
-          sx={{
-            display: 'flex',
-            flexGrow: 1,
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <PhotoAlbumOutlinedIcon
+      {albums.status === Status.Finished ? (
+        albums.data.length === 0 ? (
+          <Box
             sx={{
-              fontSize: 160,
-              color: colors.light.iconNoPhotoYet,
-            }}/>
-          <Typography variant="subtitle1" component={'div'} sx={{ textAlign: 'center', color: colors.light.textSecondary }}>
-            There are no albums yet. Please <br /> click{' '}
-            <Typography variant="subtitle2" component="span">
-              Upload album
-            </Typography>{' '}
-            to add
-          </Typography>
-        </Box>
+              display: 'flex',
+              flexGrow: 1,
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <PhotoAlbumOutlinedIcon
+              sx={{
+                fontSize: 160,
+                color: colors.light.iconNoPhotoYet,
+              }}
+            />
+            <Typography variant="subtitle1" component={'div'} sx={{ textAlign: 'center', color: colors.light.textSecondary }}>
+              There are no albums yet. Please <br /> click{' '}
+              <Typography variant="subtitle2" component="span">
+                Upload album
+              </Typography>{' '}
+              to add
+            </Typography>
+          </Box>
+        ) : (
+          <Grid container spacing={3}>
+            {albums.data.map((item) => {
+              return (
+                <Grid item key={item.id}>
+                  <AlbumCard title={item.title} idPhoto={item.photos[0]} countPhotos={item.photos.length} />
+                </Grid>
+              );
+            })}
+          </Grid>
+        )
       ) : (
-        <Grid container spacing={3}>
-          {albums.data.map((item) => {
-            return (
-              <Grid item key={item.id}>
-                <AlbumCard title={item.title} idPhoto={item.photos[0]} countPhotos={item.photos.length} />
-              </Grid>
-            );
-          })}
-        </Grid>
+        <Box></Box>
       )}
     </>
   );
--- src/modules/pages/AllPhoto.tsx
+++ src/modules/pages/AllPhoto.tsx
@@ -16,42 +16,45 @@ const AllPhoto = (): JSX.Element => {
       dispatch(clearPhotos());
     };
   }, [dispatch]);
+
   return (
     <>
-      {allPhotos.data.length !== 0 && allPhotos.status === Status.Finished ? (
-        <Box>
-          <ImageList gap={8} cols={6}>
-            {allPhotos.data.map((item) => (
-              <ImageListItem key={item.id}>
-                <img src={`data:image/jpeg;base64,${item.image}`} alt={item.description} />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        </Box>
-      ) : (
-        <Box
-          sx={{
-            display: 'flex',
-            flexGrow: 1,
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <ImageOutlinedIcon
+      {allPhotos.status === Status.Finished ?
+        allPhotos.data.length!==0?(
+          <Box>
+            <ImageList gap={8} cols={6}>
+              {allPhotos.data.map((item) => (
+                <ImageListItem key={item.id}>
+                  <img src={`data:image/jpeg;base64,${item.image}`} alt={item.description} />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Box>
+        ) : (
+          <Box
             sx={{
-              fontSize: 160,
-              color: colors.light.iconNoPhotoYet,
-            }}/>
-          <Typography variant="subtitle1" component={'div'} sx={{ textAlign: 'center', color: colors.light.textSecondary }}>
-            There are no photos yet. Please <br /> click{' '}
-            <Typography variant="subtitle2" component="span">
-              Upload photo
-            </Typography>{' '}
-            to add
-          </Typography>
-        </Box>
-      )}
+              display: 'flex',
+              flexGrow: 1,
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <ImageOutlinedIcon
+              sx={{
+                fontSize: 160,
+                color: colors.light.iconNoPhotoYet,
+              }}/>
+            <Typography variant="subtitle1" component={'div'} sx={{ textAlign: 'center', color: colors.light.textSecondary }}>
+              There are no photos yet. Please <br /> click{' '}
+              <Typography variant="subtitle2" component="span">
+                Upload photo
+              </Typography>{' '}
+              to add
+            </Typography>
+          </Box>
+        ):(<Box></Box>)
+      }
 
       <Fab variant="extended" size="medium">
         <FileUploadOutlinedIcon sx={{ mr: 1 }} />
