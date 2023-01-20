02f64077a5b07a117cc6d9888456f99ce49dbb53
alekseikorotkov <korotkovaleksei@icloud.com>
2023-01-20 19:45:41 +0200

    Implement PageTemplate component

---
+++ src/modules/components/PageTemplate/PageTemplate.tsx
@@ -0,0 +1,21 @@
+import { Box, Container, Divider } from '@mui/material';
+import { colors } from 'styles/variables';
+
+const PageTemplate = ({ header, body }: { header: JSX.Element; body: JSX.Element }): JSX.Element => {
+  return (
+    <Box>
+      <Container disableGutters>
+        <Box sx={{ height: 64 }}>
+          {header}
+        </Box>
+      </Container>
+      <Divider sx={{ bgcolor: colors.light.divider, borderColor: colors.light.divider }} />
+      <Container disableGutters>
+        {body}
+      </Container>
+
+    </Box>
+  );
+};
+
+export default PageTemplate;
