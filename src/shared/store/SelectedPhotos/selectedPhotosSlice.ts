2a22adcfab3e005a8dc9fba9617178d60aa9855c
alekseikorotkov <korotkovaleksei@icloud.com>
2023-01-23 00:02:12 +0200

    Refactor selectedPhotosSlice

--- src/shared/store/SelectedPhotos/selectedPhotosSlice.ts
+++ src/shared/store/SelectedPhotos/selectedPhotosSlice.ts
@@ -1,20 +1,20 @@
 import { IPhoto } from './../../interfaces/photo.interface';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-
-
-
-const initialState: IPhoto[] = [];
+const initialState: { isShow: boolean; data: IPhoto[] } = { isShow: false, data: [] };
 
 const selectedPhotosSlice = createSlice({
   name: '@@selectedPhotos',
   initialState,
   reducers: {
     clearSelectedPhotos: () => initialState,
-    addToSelectedPhotos: (state, action: PayloadAction<IPhoto>) => { state.push(action.payload); },
-    removeFromSelectedPhotos: (state, action: PayloadAction<IPhoto>) => { return state.filter((item) => item.id !== action.payload.id); },
+    addToSelectedPhotos: (state, action: PayloadAction<IPhoto>) => { state.data.push(action.payload); },
+    removeFromSelectedPhotos: (state, action: PayloadAction<IPhoto>) => {
+      state.data = state.data.filter((item) => item.id !== action.payload.id);
+    },
+    toggleIsShow: (state, action: PayloadAction<boolean>) => { state.isShow = action.payload; },
   },
 });
 
-export const { clearSelectedPhotos, addToSelectedPhotos, removeFromSelectedPhotos } = selectedPhotosSlice.actions;
+export const { clearSelectedPhotos, addToSelectedPhotos, removeFromSelectedPhotos, toggleIsShow } = selectedPhotosSlice.actions;
 export default selectedPhotosSlice.reducer;
