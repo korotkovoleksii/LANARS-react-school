93e477d3b9c52bc71de50b8288784db40596b742
alekseikorotkov <korotkovaleksei@icloud.com>
2023-01-22 21:02:43 +0200

    Fix name and small refactoring selectedPhotosSlice

--- src/shared/store/SelectedPhotos/selectedPhotosSlice.ts
+++ src/shared/store/SelectedPhotos/selectedPhotosSlice.ts
@@ -7,14 +7,14 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 const initialState: IPhoto[] = [];
 
 const selectedPhotosSlice = createSlice({
-  name: '@@photo',
+  name: '@@selectedPhotos',
   initialState,
   reducers: {
-    clearSelectedPhoto: () => initialState,
-    addToSelectedPhoto: (state, action: PayloadAction<IPhoto>) => { state.push(action.payload); },
-    removeFromSelectedPhoto: (state, action: PayloadAction<IPhoto>) => { return state.filter((item) => item.id !== action.payload.id); },
+    clearSelectedPhotos: () => initialState,
+    addToSelectedPhotos: (state, action: PayloadAction<IPhoto>) => { state.push(action.payload); },
+    removeFromSelectedPhotos: (state, action: PayloadAction<IPhoto>) => { return state.filter((item) => item.id !== action.payload.id); },
   },
 });
 
-export const { clearSelectedPhoto, addToSelectedPhoto, removeFromSelectedPhoto } = selectedPhotosSlice.actions;
+export const { clearSelectedPhotos, addToSelectedPhotos, removeFromSelectedPhotos } = selectedPhotosSlice.actions;
 export default selectedPhotosSlice.reducer;
